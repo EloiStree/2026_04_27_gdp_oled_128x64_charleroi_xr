bdb0389a743a322c40392072707ba468d67e2e1d
Nos_ave <nos@ik.me>
2026-05-13 14:20:39 +0200

    add ScreenBuilder

--- team/e10_laurent/scripts/e10_screen_builder_hello_world.gd
+++ team/e10_laurent/scripts/e10_screen_builder_hello_world.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 	
 	array.resize(128 * 64)
 	#await get_tree().create_timer(1).timeout
-	var position: Vector2i = Vector2i(20, 20)
+	var position: Vector2i = Vector2i(32, 28)
 	E13ScreenBuilderPrint6x8.print_text_6x8_at_lrtd(array, position, "Hello World", true, true)
 	
 	screen.set_value_with_1d_array_and_draw(array)
