d0fc0e7c0842e84b31d867391645e23991438140
miler71 <jeaemile50@gmail.com>
2026-05-13 16:33:33 +0200

    com

--- team/e8_emile/script/e8_emile.gd
+++ team/e8_emile/script/e8_emile.gd
@@ -11,8 +11,7 @@ func _ready() -> void:
 	var position: Vector2i = Vector2i(10,10)
 	E13ScreenBuilderPrint6x8.print_text_6x8_at_lrtd(array, position, "Hello World", true, true)
 	
-	screen.set_value_with_1d_array_and_draw(array)
-	await get_tree().create_timer(0.01).timeout
+	screen.set_value_with_1d_array_and_draw()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
